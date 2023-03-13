664ed6e331120d721291ff1a789fd3da97c28110
lalkl5 <lalkl542@gmail.com>
2023-03-13 11:59:35 +0000

    fix

--- public/main.js
+++ public/main.js
@@ -1,18 +1,10 @@
 function myFunction() {
-<<<<<<< HEAD
-  alert("ПАШЕЛ нннау!");
-=======
   alert("Короче меченый, снизу есть чат. Пиши имя и сообщения, можешь общаться с чуваками кто сидит щас на сайте. \n Если че - пиши мне в вк, кнопка под чатом");
->>>>>>> 4039b08 (prod)
 }
 document.getElementById("myButton").addEventListener("click", function() {
   window.open("https://www.vk.com/lalkl4", "_blank");
 });
-<<<<<<< HEAD
-var socket = new WebSocket("ws://188.235.122.214:228/ws");
-=======
 var socket = new WebSocket("ws://smartsshtopor.com:80/ws");
->>>>>>> 4039b08 (prod)
 var userName = null;
 
 socket.onopen = function(event) {
@@ -36,11 +28,6 @@ function sendMessage() {
   const messageInput = document.getElementById("messageInput");
   const messageText = messageInput.value;
 
-<<<<<<< HEAD
-  // If userName is not set, set it to the value of nameInput
-  if (!userName) {
-    userName = nameInput.value;
-=======
   // If userName is not set or is empty, prompt the user to enter a name
   if (!userName || userName.trim() === '') {
     userName = nameInput.value.trim();
@@ -52,7 +39,6 @@ function sendMessage() {
       }
       nameInput.value = userName;
     }
->>>>>>> 4039b08 (prod)
     nameInput.disabled = true;
   }
 
